b747467f3508dc24df934f518beed85f5fd3b498
everestyeti <lucas.kwasniak@gmail.com>
2019-07-05 10:43:18 +0200

    template for app

---
+++ rest_tutorial/src/main/java/comarch/rest_tutorial/repository/PetsRepository.java
@@ -0,0 +1,9 @@
+package comarch.rest_tutorial.repository;
+
+import comarch.rest_tutorial.models.Pets;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PetsRepository extends MongoRepository<Pets, String> {
+    Pets findBy_id(ObjectId _id);
+}
