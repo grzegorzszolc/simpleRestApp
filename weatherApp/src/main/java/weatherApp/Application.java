18064d8c0bb725e6a18e02bcec50f8f4da9ae01c
jseta97 <jseta97@gmail.com>
2019-07-05 17:14:11 +0200

    WeatherModel, example of get data from api.openweathermap.org

--- weatherApp/src/main/java/weatherApp/Application.java
+++ weatherApp/src/main/java/weatherApp/Application.java
@@ -1,13 +1,31 @@
 package weatherApp;
 
+import com.google.gson.Gson;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+import weatherApp.models.WeatherModel;
+import java.util.Collections;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		String resourceUrl = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=eb9e1c4a0134d4f7655c89bff7d0a371";
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		ResponseEntity<String> response = restTemplate.exchange(resourceUrl, HttpMethod.GET,entity,String.class);
+		Gson g = new Gson();
+		WeatherModel weatherModel = g.fromJson(response.getBody(), WeatherModel.class);
+		System.out.println(response);
+	}
 }
